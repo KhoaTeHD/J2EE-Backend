64ccaa01fff8abd0453ef4c8764c93496ed0523a
KhoaTeHD <dangkhoa014@gmail.com>
2023-11-24 13:33:37 +0700

    Khoa up

--- SocialMedia/src/main/java/com/groupnine/mediasocial/security/SecurityContext.java
+++
@@ -1,7 +0,0 @@
-//package com.groupnine.mediasocial.security;
-//
-//public class SecurityContext {
-//	
-//	public static final String JWT_KEY="voperuihjdfbhwevrhvzierkmnerebmweoirunezokoidrenizhjeb";
-//	public static final String HEADER="Authorization";
-//}
