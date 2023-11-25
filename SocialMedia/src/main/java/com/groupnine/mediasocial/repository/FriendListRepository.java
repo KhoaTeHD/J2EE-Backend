90a6cf558f50f84ec7b133dfb73dcd09f661ab51
TeHD <dangkhoa014@gmail.com>
2023-11-25 12:28:07 +0700

    test

---
+++ SocialMedia/src/main/java/com/groupnine/mediasocial/repository/FriendListRepository.java
@@ -0,0 +1,9 @@
+package com.groupnine.mediasocial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.groupnine.mediasocial.entity.FriendList;
+
+public interface FriendListRepository extends JpaRepository<FriendList, Long> {
+
+}
--- SocialMedia/src/main/java/com/groupnine/mediasocial/service/UserService.java
+++ SocialMedia/src/main/java/com/groupnine/mediasocial/service/UserService.java
@@ -1,23 +1,19 @@
-//package com.groupnine.mediasocial.service;
-//
-//import java.util.List;
-//import org.springframework.stereotype.Service;
-//import com.groupnine.mediasocial.entity.User;
-//import com.groupnine.mediasocial.exception.UserException;
-//
-//@Service
-//public interface UserService {
-//
-//	public User registerUser(User user) throws UserException;
-//	public User findUserById(Long userId) throws UserException;
-//	public User findUserProfile(String token) throws UserException;
-//	public User findUserByUsername(String username) throws UserException;
-//	
-//	public List<User> findUserByIds(List<Long> userIds) throws UserException;
-//	
-//	public List<User> searchUser(String query) throws UserException;
-//	
-//	public User updateUserDetails(User updatedUser, User existingUser) throws UserException;
-//	
-//	
-//}
+package com.groupnine.mediasocial.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.groupnine.mediasocial.entity.User;
+import com.groupnine.mediasocial.exception.UserException;
+
+@Service
+public interface UserService {
+
+	public User findUserById(Long userId) throws UserException;
+	
+	public List<User> findUserByIds(List<Long> userIds) throws UserException;
+	
+	public List<User> searchUser(String query) throws UserException;
+	
+	public int getMutualFriends(Long CurrentUserId, Long targetUserId) throws UserException;
+	
+}
