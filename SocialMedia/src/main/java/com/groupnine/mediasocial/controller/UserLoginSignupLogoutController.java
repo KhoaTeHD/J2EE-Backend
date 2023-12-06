e5b7cbb38199ac45623919358f8ff9406b4abd37
KhoaTeHD <dangkhoa014@gmail.com>
2023-12-06 20:33:09 +0700

    fix conflict

--- SocialMedia/src/main/java/com/groupnine/mediasocial/controller/UserLoginSignupLogoutController.java
+++ SocialMedia/src/main/java/com/groupnine/mediasocial/controller/UserLoginSignupLogoutController.java
@@ -80,7 +80,7 @@ public class UserLoginSignupLogoutController {
 
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-		
+
 		Optional<User> userCheck = userRepository.existsByGmail(signUpRequest.getGmail());
 		if (!userCheck.isEmpty()) {
 			return ResponseEntity.badRequest().body(new MessageResponse("Error: Gmail này đã tồn tại!"));
@@ -102,8 +102,4 @@ public class UserLoginSignupLogoutController {
 
 		return ResponseEntity.ok(new MessageResponse("Đăng ký thành công!"));
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 37eee71684ec344fe99496d79440b1f0ec21bbbc
 }
