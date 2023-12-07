e6f173ee55b20ed84935e95f87a8a54f32e1df94
nnhuy2903 <nnhuy2903@gmail.com>
2023-12-07 23:59:42 +0700

    Backend

--- SocialMedia/src/main/java/com/groupnine/mediasocial/repository/ReactionRepository.java
+++ SocialMedia/src/main/java/com/groupnine/mediasocial/repository/ReactionRepository.java
@@ -1,11 +1,16 @@
 package com.groupnine.mediasocial.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.groupnine.mediasocial.entity.Reaction;
 
 @Repository
 public interface ReactionRepository extends JpaRepository<Reaction, Long>{
-
+	
+	@Query(value = "SELECT * from reaction r where r.user_id = :userId and r.post_id = :postId", nativeQuery = true)
+	Optional<Reaction> findByUserIdAndPostId(Long userId, Long postId);
 }
